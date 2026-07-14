85adfc85fb2294f2e1bcf5328497a72875f83730
Ranjit Budhathoki <ranjitbudhathoki@lftechnology.com>
2026-07-14 17:13:54 +0545

    nvim and tmux

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Diff View" })
+
+vim.keymap.set("n", "<leader>fF", function()
+  Snacks.picker.files({ hidden = true, no_ignore = true })
+end, { desc = "Find Files (hidden)" })
