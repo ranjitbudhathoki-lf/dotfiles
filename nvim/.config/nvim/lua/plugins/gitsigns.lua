58c81b0d91fbf73d81331fa2406f52ccc9947481
Ranjit Budhathoki <ranjitbudhathoki@lftechnology.com>
2026-07-14 21:37:34 +0545

    add gitsigns nvim

---
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,10 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 0, -- no delay, shows immediately as you move
+      virt_text_pos = "eol",
+    },
+  },
+}
